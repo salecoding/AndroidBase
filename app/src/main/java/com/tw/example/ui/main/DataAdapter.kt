dbd303067c7c854cfd12d941aff004ae3e338f8e
salecoding <salecoding@gmail.com>
2019-05-17 16:45:36 +0800

    edit adapter

--- app/src/main/java/com/tw/example/ui/main/DataAdapter.kt
+++ app/src/main/java/com/tw/example/ui/main/DataAdapter.kt
@@ -10,9 +10,9 @@ class DataAdapter : BaseQuickAdapter<TestEntity, BaseViewHolder>(R.layout.item_d
 
     override fun convert(helper: BaseViewHolder, item: TestEntity) {
         helper.setText(R.id.tvSourceName, item.zfactoryname)
-        helper.setText(R.id.tvTotal, String.format("任务总数：%s", item.total))
-        helper.setText(R.id.tvUntreated, String.format("未处理：%s", item.untreated))
-        helper.setText(R.id.tvOverdue, String.format("已逾期：%s", item.overdue))
-        helper.setText(R.id.tvProcessed, String.format("已处理：%s", item.processed))
+        helper.setText(R.id.tvTotal, "任务总数${item.total}")
+        helper.setText(R.id.tvUntreated, "未处理：${item.untreated}")
+        helper.setText(R.id.tvOverdue, "已逾期：${item.overdue}")
+        helper.setText(R.id.tvProcessed, "已处理：${item.processed}")
     }
 }
